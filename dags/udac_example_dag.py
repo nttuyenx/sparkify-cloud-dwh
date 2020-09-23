f32286d74b62200f0191b660b191ae2dcd6ed387
Nguyen The Tuyen <nttuyenx@gmail.com>
2020-09-24 06:40:20 +0700

    Update Fact Operator

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -30,29 +30,32 @@ start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id="Stage_events",
+    dag=dag,
     table="staging_events",
     redshift_conn_id="redshift",
     aws_credentials_id="aws_credentials",
     s3_bucket="udacity-dend",
     s3_key="log_data",
-    jsonpath="s3://udacity-dend/log_json_path.json",
-    dag=dag
+    jsonpath="s3://udacity-dend/log_json_path.json"
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
+    dag=dag,
     table="staging_songs",
     redshift_conn_id="redshift",
     aws_credentials_id="aws_credentials",
     s3_bucket="udacity-dend",
     s3_key="song_data",
-    jsonpath="auto",
-    dag=dag
+    jsonpath="auto"
 )
 
 load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="songplays",
+    sql_stmt=SqlQueries.songplay_table_insert
 )
 
 load_user_dimension_table = LoadDimensionOperator(
--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -2,21 +2,33 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class LoadFactOperator(BaseOperator):
 
     ui_color = '#F98866'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table="",
+                 sql_stmt="",
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
         # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql_stmt = sql_stmt
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info("Clearing data from destination Redshift table")
+        redshift.run("DELETE FROM {}".format(self.table))
+
+        self.log.info("Inserting data into fact table")
+        formated_sql = """
+            INSERT INTO {}
+            {}
+        """.format(self.table, self.sql_stmt)
+        redshift.run(formated_sql)
