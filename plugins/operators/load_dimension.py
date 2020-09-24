9ba115152f40d8f2f66797b9bd9e468962dac798
Nguyen The Tuyen <nttuyenx@gmail.com>
2020-09-24 08:46:23 +0700

    Update Dimension Operator

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -2,6 +2,7 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
@@ -9,14 +10,28 @@ class LoadDimensionOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table="",
+                 sql_stmt="",
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql_stmt = sql_stmt
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info("Clearing data from destination Redshift table")
+        redshift.run("DELETE FROM {}".format(self.table))
+
+        self.log.info(
+            "Inserting data into dimension table '{}'".format(self.table)
+        )
+        formated_sql = """
+            INSERT INTO {}
+            {}
+        """.format(self.table, self.sql_stmt)
+        redshift.run(formated_sql)
